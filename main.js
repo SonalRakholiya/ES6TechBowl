21c30f1076d52596fa52fce545ee5b63f7746a73
jaimishakoldiya <jemishakoladiya2001@gmail.com>
2021-04-27 09:48:50 +0530

    change in js

--- main.js
+++ main.js
@@ -42,3 +42,93 @@ getInfo(`graps`, 100, "Green");;
 //     )}
 // </ul>
 
+// Rest Parameters
+
+function add(...args) {
+    return args.filter((num) => typeof num === "number")
+        .reduce((prev, curr) => prev + curr);
+}
+const result = add(10, "hello", null, undefined, 20);
+console.log(result);
+
+
+
+let fruits = new Map();
+fruits.set("apple", "Red");
+fruits.set("Banana", "Yellow");
+fruits.set("Graps", "Green");
+for (const fruit of fruits) {
+    console.log(fruit);
+
+}
+
+//single tone in java script
+let student = new (class {
+    constructor(name) {
+        this.name = name;
+    }
+    getname() {
+        console.log(`My name is ${this.name}`);
+    }
+})("sonal pipaliya");
+
+student.getname();
+
+// gettter Setter
+class students {
+    constructor(firstName, lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    get fullName() {
+        return this.firstName + " " + this.lastName;
+    }
+
+    set fullName(str) {
+        let names = str.split(' ');
+        if (names.length === 2) {
+            this.firstName = names[0];
+            this.lastName = names[1];
+        }
+        else {
+            throw "Invalid name formate";
+        }
+    }
+}
+let uname = new students("Sonal", "Pipaliya");
+console.log(uname.fullName);
+
+uname.fullName = "Paras Bhesaniya";
+console.log(uname.fullName);
+
+
+class Queue extends Array {
+    enqueue(element) {
+        super.push(element);
+    }
+    dequeue() {
+        super.shift();
+    }
+    peek() {
+         return !this.empty() ? this[0] : undefined;
+    }
+    empty() {
+        return this.length === 0;
+    }
+}
+let chars = new Queue()
+chars.enqueue("Sonal");
+chars.enqueue("hardik");
+chars.enqueue("paras");
+console.log(chars);
+
+while (!chars.empty()) {
+    chars.dequeue();
+}
+console.log(chars);
+chars.peek();
+console.log(chars);
+chars.empty();
+console.log(chars);
+
